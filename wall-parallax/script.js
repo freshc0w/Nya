77c8525ce491afe0536cae82abea1d0bf50b10da
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-03-25 11:43:11 +0000

    Add pics and vids

--- wall-parallax/script.js
+++ wall-parallax/script.js
@@ -1,11 +1,26 @@
 const imgClasses = document.querySelectorAll(".img");
 
+let imgCount = 1;
+let vidCount = 1;
 imgClasses.forEach((img) => {
 	// console.log(img.src);
 
 	// Add shading effect
 	console.log(img.tagName);
+	if(img.tagName === "IMG") {
+		img.src = `./images/${imgCount}.jpg`;
+		imgCount++;
+		if(imgCount === 68) imgCount = 1;
+	} else {
+		img.src = `./videos/video${vidCount}.mp4`;
+		vidCount++;
+		if(vidCount === 13) vidCount = 1;
+	}
+
+
 	img.tagName === "IMG"
 		? (img.parentNode.style = `background-image: url(${img.src})`)
 		: img.parentNode.style = `background-image: url(./images/spiral-rainbow-background.jpg)`;
 });
+
+
